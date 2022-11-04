6bb301c3204367d2403df4c550089b900de3909e
kitty <kitt.horvath6891@gmail.com>
2022-11-04 11:02:32 +0100

    permissions.py and views.py added

--- backend/answers/views.py
+++ backend/answers/views.py
@@ -1,14 +1,18 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
 from answers.models import Answers
+from answers.permissions import ReadOnly
 from answers.serializers import AnswersSerializer
 
 
 class ListCreateAnswersView(ListCreateAPIView):
     queryset = Answers.objects.all()
     serializer_class = AnswersSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class RetrieveUpdateDestroyAnswersView(RetrieveUpdateDestroyAPIView):
     queryset = Answers.objects.all()
     serializer_class = AnswersSerializer
+    permission_classes = [IsAdminUser | ReadOnly]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
 from comments.models import Comments
+from comments.permissions import ReadOnly
 from comments.serializers import CommentsSerializer
 
 
 class ListCreateCommentsView(ListCreateAPIView):
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class RetrieveUpdateDestroyCommentsView(RetrieveUpdateDestroyAPIView):
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
+    permission_classes = [IsAdminUser | ReadOnly]
 
