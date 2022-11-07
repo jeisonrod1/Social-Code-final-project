0da25f754d3493209b08933bf5515a19a65f94cf
Mads <mads.jensen@evadoo.ai>
2022-11-07 18:03:04 +0100

    post-card-layout

--- frontend/src/components/Posts/index.js
+++ frontend/src/components/Posts/index.js
@@ -3,16 +3,50 @@ import card1 from "../../images/restaurants/card-1.jpg";
 import card2 from "../../images/restaurants/card-2.jpg";
 import card3 from "../../images/restaurants/card-3.jpg";
 import card4 from "../../images/restaurants/card-4.jpg";
-import CardPost from "./CardPost/index.js";
+import CardMidPost from "./CardMidPost/index.js";
+import CardMidNewPost from "./CardMidNewPost/index.js";
+import CardRightNav from "./CardRightNav/index.js";
+import CardLeftNav from "./CardLeftNav/index.js";
 
 // STYLED COMPONENTS -start
 
-const CardContainer = styled.div`
-  width: 870px;
+const RowWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 1000px;
+  margin: auto;
+  padding-top: 10px;
+`;
+
+const CardContainerLeft = styled.div`
+  width: 180px;
+  height: 100%;
+  position: sticky;
+  top: 0;
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
-  padding-top: 26px;
+  padding-top: 62px;
+`;
+
+const CardContainerMid = styled.div`
+  width: 600px;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: 62px;
+`;
+
+const CardContainerRight = styled.div`
+  width: 220px;
+  height: 100%;
+  position: sticky;
+  top: 0;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: 62px;
 `;
 
 const Navbar = styled.div`
@@ -37,29 +71,22 @@ const Navbar = styled.div`
 const Posts = () => {
   return (
     <>
-      <h1>Posts</h1>
-      <h1>Posts</h1>
-      <h1>Posts</h1>
-      <CardContainer>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-        <CardPost/>
-      </CardContainer>
+      <RowWrapper>
+        <CardContainerLeft>
+          <CardLeftNav />
+        </CardContainerLeft>
+        <CardContainerMid>
+          <CardMidNewPost />
+          <CardMidPost />
+          <CardMidPost />
+          <CardMidPost />
+          <CardMidPost />
+          <CardMidPost />
+        </CardContainerMid>
+        <CardContainerRight>
+          <CardRightNav />
+        </CardContainerRight>
+      </RowWrapper>
     </>
   );
 };
