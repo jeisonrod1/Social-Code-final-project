560bb93a04e34c7a129681f8fc16cce9a073c4f0
A.honegger <alexanderhonegger@hotmail.com>
2022-11-08 15:18:10 +0100

    CSRF AlLOWE ALL ORIGINS

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--48f5ng%++^e*xdfoe*bsmd09lzbc0gth7n&pxzo-hjj+%=i0-
 DEBUG = os.environ.get('DJANGO_DEBUG', "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://code-media.propulsion-learn.ch/", 'http://127.0.0.1', "http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 
