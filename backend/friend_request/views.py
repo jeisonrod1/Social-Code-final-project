49b86a345bd1a52f0599400ccb4a0c52683795bf
kitty <kitt.horvath6891@gmail.com>
2022-11-08 14:51:54 +0100

    send friend request function added to Friend Request app

--- backend/friend_request/views.py
+++ backend/friend_request/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from friend_request.models import FriendRequest
+
+
+@login_required
+def send_friend_request(request, userID):
+    friend_request_requester = request.user
+    friend_request_receiver =User.objects.get(id=userID)
+    fiend_request, created = FriendRequest.objects.get_or_create(
+        from_user=friend_request_requester, to_user=friend_request_receiver
+    )
+    if created:
+        return HttpResponse('friend request sent')
+    else:
+        return HttpResponse('friend request was already sent')
+
