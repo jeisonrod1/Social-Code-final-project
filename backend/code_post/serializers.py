9ff9a4f8cd7b3da3954bb2c1bd7a7496ca4322ae
kitty <kitt.horvath6891@gmail.com>
2022-11-08 10:58:40 +0100

    core backend for CodePosts, Comments, Answers, GeneralPosts is completed.

--- backend/code_post/serializers.py
+++ backend/code_post/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
 from code_post.models import CodePost
+from user.serializers import UserSerializer
 
 
 class CodePostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CodePost
+        fields = '__all__'
+        depth = 1
+
+    user = UserSerializer
+
+
+
+class CreateCodePostSerializer(serializers.ModelSerializer):
     class Meta:
         model = CodePost
         fields = '__all__'
