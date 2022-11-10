a82b3a0aff581de3b22b60c7bc26fb9d58eaaeda
A.honegger <alexanderhonegger@hotmail.com>
2022-11-10 17:29:12 +0100

    Working on points system

--- backend/wallet/models.py
+++ backend/wallet/models.py
@@ -6,16 +6,12 @@ from badges.models import Badge
 User = get_user_model()
 
 
-def badge_image_directory_path(instance, filename):
-    return f'badge/{instance.id}/{filename}'
-
-
 class Wallet(models.Model):
     TYPE_CHOICES = (
         ('Badger', 'Badger'),
         ('404-error', '404-error'),
     )
-    type = models.CharField(max_length=100, choices=TYPE_CHOICES)
+    name = models.CharField(max_length=100, choices=TYPE_CHOICES)
     amount = models.IntegerField(default=0)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='wallet')
     badges = models.ManyToManyField(to=Badge, related_name='wallet', blank=True)
--- backend/wallet/views.py
+++ backend/wallet/views.py
@@ -1,9 +1,12 @@
-from rest_framework.generics import ListCreateAPIView, UpdateAPIView, RetrieveAPIView, ListAPIView
+from django.contrib.auth import get_user_model
+from rest_framework.generics import ListCreateAPIView, UpdateAPIView, RetrieveAPIView, ListAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
-
+from user.serializers import UserSerializer
 from wallet.models import Wallet
 from wallet.serializers import CreateWalletSerializer, BuyBadgeSerializer, GetWalletSerializer
+User = get_user_model()
 
 
 class ListCreateWalletView(ListCreateAPIView):
@@ -22,13 +25,28 @@ class BuyBadge(UpdateAPIView):
     lookup_field = 'id'
 
 
+class Payment(GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def patch(self, request, *args, **kwargs):
+        instance = self.request.user
+        serializer = UserSerializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
 class GetCurrentWalletAmount(ListAPIView):
     serializer_class = GetWalletSerializer
     queryset = Wallet.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        queryset = Wallet.objects.filter(user=self.request.user.id)
+        user = self.request.user
+        a = self.request.data.get('name')
+        queryset = Wallet.objects.filter(user=user)
+        queryset = queryset.filter(name=a)
         return queryset
 
 
