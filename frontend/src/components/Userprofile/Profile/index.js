fcfb4ad6e78ffa23f0dd3af68f47333276b53914
A.honegger <alexanderhonegger@hotmail.com>
2022-11-11 15:58:37 +0100

    USER PROFILE FETCHES

--- frontend/src/components/Userprofile/Profile/index.js
+++ frontend/src/components/Userprofile/Profile/index.js
@@ -23,6 +23,8 @@ import {
 } from "./index.styled";
 import ProfileBadges from "../ProfileBadges";
 import TopPosts from "../TopPosts";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 // STYLED COMPONENTS -start
 
@@ -31,15 +33,77 @@ const ExampleComponent = styled.div``;
 // STYLED COMPONENTS -end
 
 const Userprofile = () => {
+    const [token, setToken] = useState(localStorage.getItem("auth"))
+    const [username, setUsername] = useState("")
+    const [first_name, setFirst_Name] =  useState("")
+    const [last_name, setLast_Name] =  useState("")
+    const [email, setEmail] =  useState("")
+    const [location, setLocation] =  useState("")
+    const [phone, setPhone] =  useState("")
+    const [about_me, setAbout_Me] =  useState("")
+    const [avatar, setAvatar] =  useState("")
+    const [points, setPoints] =  useState("")
+    const [company, setCompany] =  useState("")
+
+    const fetchProfile = () => {
+
+        const url = "https://code-media.propulsion-learn.ch//backend/api/social/users/me/"
+
+
+        const config = {
+            method: "GET",
+            headers: new Headers({
+                "Autorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY4NjA4ODQzLCJpYXQiOjE2NjgxNzY4NDMsImp0aSI6ImQ5MWU4ZmM0YWM4NDQwNDA5ZjdkNDY4MTc1MzQ0NmZjIiwidXNlcl9pZCI6MX0.Qf_-28v18BwVVq92Cn8sjKTDFPY8DzpNopR1WbM2p3o"
+            })
+        }
+
+        fetch(url, config)
+        .then((response) => {
+            if (response.status === 200) {
+                const data = response.json();
+                setUsername(data.get('username'));
+                setFirst_Name(data.get('first_name'));
+                setLast_Name(data.get('last_name'));
+                setEmail(data.get('email'));
+                setLocation(data.get('location'));
+                setPhone(data.get('phone'));
+                setAbout_Me(data.get('about_me'));
+                setAvatar(data.get('avatar'));
+                setPoints(data.get('points'));
+                setCompany(data.get('company'));
+
+
+            }
+            else {
+                console.log(response.json())
+            }
+        })
+        .then(data => { setToken(data.access)
+            console.log(token)});
+
+
+    }
+
+
+     useEffect(() => {
+       fetchProfile()
+
+      }, [token]);
+
+
+
+
+
+
   return (
     <ProfilePage>
       <UserHeader>
-        <ProfilePicture src={ProfileAvatar} />
+        <ProfilePicture src={avatar} />
         <UserInfo>
-          <UserName>Victor Kaufmann</UserName>
+          <UserName>{username}</UserName>
           <UserOccupation>Software Engineer</UserOccupation>
-          <UserLocation>Zürich, CH.</UserLocation>
-          <UserEmail>victor.fmann@gmail.com</UserEmail>
+          <UserLocation>{location}</UserLocation>
+          <UserEmail>{email}</UserEmail>
         </UserInfo>
       </UserHeader>
       <StatsContainer>
@@ -54,9 +118,7 @@ const Userprofile = () => {
       <AboutContainer>
         <AboutTitle>About</AboutTitle>
         <About>
-          I am a computer science graduate and a programmer, I like to take up
-          projects for learning 😅 , kinds geeky, and trying to get my hands in
-          python.
+            {about_me}
         </About>
       </AboutContainer>
       <div></div>
