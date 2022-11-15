81e87951ed01b571dd5135e69e4ac911b073ec76
A.honegger <alexanderhonegger@hotmail.com>
2022-11-15 09:29:45 +0100

    Minor changes to posts component

--- frontend/src/components/CreatePost/CreateCodePost/index.js
+++ frontend/src/components/CreatePost/CreateCodePost/index.js
@@ -13,7 +13,7 @@ import {
   SubmitButtonButton,
   SubmitButtonDiv,
   TitleDiv,
-  InputItemSelectField,
+  InputItemSelectField, OutputItemDiv, OutputCodeDiv,
 } from "./index.styled.js";
 import { useNavigate } from "react-router-dom";
 import Editor from "@monaco-editor/react";
@@ -160,6 +160,12 @@ function CreatePost() {
               onChange={handleCodeChange}
             />
           </InputItemDiv>
+          <OutputItemDiv>
+              <OutputCodeDiv>
+
+              </OutputCodeDiv>
+
+          </OutputItemDiv>
 
           
           <SubmitButtonDiv>
